3dd246ba4899649054e0e41dcebd9bc4b7ca380a
Binisha <binisha072@gmail.com>
2025-04-09 22:16:56 +0545

    brouser router addded

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import Register from "./pages/register";
 
 import { TaskProvider } from "./context/TaskContext";
 import Dashboard from "./pages/dashboard";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <AuthProvider>
       <TaskProvider>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/tasks" element={<Dashboard />} />
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/tasks" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
       </TaskProvider>
     </AuthProvider>
   );
